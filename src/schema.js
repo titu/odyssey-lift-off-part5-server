5e726cf7fe0c4bdc7614b25139941780b4751fb7
titu <email.to.titu@gmail.com>
2022-01-30 17:52:54 +0600

    durationInSeconds added

--- src/schema.js
+++ src/schema.js
@@ -35,14 +35,16 @@ const typeDefs = gql`
     author: Author!
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
-    "The track's approximate length to complete, in minutes"
-    length: Int
+    "The track's approximate length to complete, in seconds"
+    length: Int @deprecated(reason: "Use durationInSeconds")
     "The number of modules this track contains"
     modulesCount: Int
     "The track's complete description, can be in markdown format"
     description: String
     "The number of times a track has been viewed"
     numberOfViews: Int
+    "The track's full duration, in seconds"
+    durationInSeconds: Int
     "The track's complete array of Modules"
     modules: [Module!]!
   }
@@ -61,12 +63,14 @@ const typeDefs = gql`
     id: ID!
     "The module's title"
     title: String!
-    "The module's length in minutes"
-    length: Int
+    "The module's length in seconds"
+    length: Int @deprecated(reason: "Use durationInSeconds")
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
     content: String
     "The module's video url, for video-based modules"
     videoUrl: String
+    "The module's video duration, in seconds"
+    durationInSeconds: Int
   }
 `;
 
